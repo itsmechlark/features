637023153b4faf95b89e5115683f24eeea9c9734
John Clark <clark@sumatra.com.ph>
2023-09-15 11:31:57 +0800

    test: Bump 1Password to v2.21

--- test/1password/version.sh
+++ test/1password/version.sh
@@ -6,7 +6,7 @@ set -e
 source dev-container-features-test-lib
 
 # Definition specific tests
-check "version" op  --version | grep 2.19
+check "version" op  --version | grep 2.21
 
 # Report result
 reportResults
