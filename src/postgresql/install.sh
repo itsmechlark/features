1c4d1e0c9971c8912068d485f2705bfff0c44383
Alexander Manzyuk <admsev@gmail.com>
2024-05-07 12:04:49 +0200

    fix(postgresql): Support IPv6

--- src/postgresql/install.sh
+++ src/postgresql/install.sh
@@ -65,6 +65,8 @@ chown -R postgres:postgres $PGDATA \
     && echo "listen_addresses = '*'" >> /etc/postgresql/${version_major}/main/postgresql.conf \
     && echo "data_directory = '$PGDATA'" >> /etc/postgresql/${version_major}/main/postgresql.conf \
     && echo "host   all all 0.0.0.0/0        trust" > /etc/postgresql/${version_major}/main/pg_hba.conf \
+    && echo "host   all all ::/0             trust" >> /etc/postgresql/${version_major}/main/pg_hba.conf \
+    && echo "host   all all ::1/128          trust" >> /etc/postgresql/${version_major}/main/pg_hba.conf \
     && sudo -H -u postgres sh -c "/usr/lib/postgresql/${version_major}/bin/initdb -D $PGDATA --auth-local trust --auth-host scram-sha-256" \
     && sudo /etc/init.d/postgresql start \
     && pg_isready -t 60
