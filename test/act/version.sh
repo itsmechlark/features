70f10abc3c9150c01d4062e7ff27e83e064716e2
Clark Sumatra <clark@sumatra.com.ph>
2025-01-18 18:13:25 +0800

    feat(act): Update version options

--- test/act/version.sh
+++ test/act/version.sh
@@ -6,7 +6,7 @@ set -e
 source dev-container-features-test-lib
 
 # Definition specific tests
-check "version" act --version | grep 0.2.64
+check "version" act --version | grep 0.2.71
 
 # Report result
 reportResults
