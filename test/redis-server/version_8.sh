f00544775b288a2b5336bb15d788b5a750fec9ab
Clark Sumatra <clark@sumatra.com.ph>
2025-07-21 15:57:00 +0800

    feat(redis-server): Support v8

--- test/redis-server/version_8.sh
+++ test/redis-server/version_8.sh
@@ -6,8 +6,8 @@ set -e
 source dev-container-features-test-lib
 
 # Definition specific tests
-check "server" redis-server -v | grep "^Redis server v=7"
-check "client" redis-cli -v | grep "^redis-cli 7"
+check "server" redis-server -v | grep "^Redis server v=8"
+check "client" redis-cli -v | grep "^redis-cli 8"
 
 # Report result
 reportResults
