033cba5070ac288102289c0a87c1aecf8894d86a
Clark Sumatra <clark@itsmechlark.com>
2025-07-21 15:49:46 +0800

    fix(redis-server): Force install redis-tools (#50)

--- test/redis-server/test.sh
+++ test/redis-server/test.sh
@@ -6,7 +6,7 @@ set -e
 source dev-container-features-test-lib
 
 # Feature specific tests
-check "pq-init-exists" bash -c "ls /usr/local/share/redis-server-init.sh"
+check "redis-init-exists" bash -c "ls /usr/local/share/redis-server-init.sh"
 
 # Report result
 reportResults
