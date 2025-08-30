d4b31774e73610af5674e027a54351c1accb3ef9
Clark Sumatra <clark@sumatra.com.ph>
2025-08-30 10:56:11 +0800

    test(postgresql): Check pg-init

--- test/postgresql/version_13.sh
+++ test/postgresql/version_13.sh
@@ -9,6 +9,7 @@ source dev-container-features-test-lib
 check "etc" ls /etc/postgresql | grep 13
 check "client" psql --version | grep 13
 check "data" sudo cat /var/lib/postgresql/data/PG_VERSION | grep 13
+check "pq-init-exists" bash -c "ls /usr/local/share/pq-init.sh"
 
 # Report result
 reportResults
