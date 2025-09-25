ae48611a04ea29deb829bb96ae298f4daf4503ed
Clark Sumatra <clark@sumatra.com.ph>
2025-09-26 00:23:05 +0800

    test(postgresql): Remove startup test

--- test/postgresql/test.sh
+++ test/postgresql/test.sh
@@ -8,10 +8,6 @@ source dev-container-features-test-lib
 # Feature specific tests
 check "etc" ls /etc/postgresql
 check "pq-init-exists" bash -c "ls /usr/local/share/pq-init.sh"
-check "init" sudo --preserve-env=PGDATA,PGHOST,PGUSER /usr/local/share/pq-init.sh /bin/true
-check "start" sudo /etc/init.d/postgresql start
-check "ready" bash -c "pg_isready -t 60"
-check "connect" bash -c "psql -U postgres -tAc 'SELECT 1;' | grep 1"
 
 # Report result
 reportResults
--- test/postgresql/version_18.sh
+++ test/postgresql/version_18.sh
@@ -10,10 +10,6 @@ check "etc" ls /etc/postgresql | grep 18
 check "client" psql --version | grep 18
 check "data" sudo cat /var/lib/postgresql/data/PG_VERSION | grep 18
 check "pq-init-exists" bash -c "ls /usr/local/share/pq-init.sh"
-check "init" sudo --preserve-env=PGDATA,PGHOST,PGUSER /usr/local/share/pq-init.sh /bin/true
-check "start" sudo /etc/init.d/postgresql start
-check "ready" bash -c "pg_isready -t 60"
-check "connect" bash -c "psql -U postgres -tAc 'SELECT 1;' | grep 1"
 
 # Report result
 reportResults
